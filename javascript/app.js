4907f1ae452feae0e48fb08831620a762da762cb
Squarewave34 <132561675+Squarewave34@users.noreply.github.com>
2024-10-16 16:46:53 +0300

    cleaning code before trying to improve inventoy

--- javascript/app.js
+++ javascript/app.js
@@ -2,41 +2,41 @@
 const script= [
   [
     {line: "this is a test line", spriteLeft:"url('../assets/Senku_test.png')", spriteRight:"url('../assets/Gen_test.png')"},
-    // {line: "this line makes sure i can keep on moving with script", spriteLeft:"url('../assets/Senku_test.png')", spriteRight:"url('../assets/Gen_test.png')"},
-    // {line: "and this is my last test for scene 0", spriteLeft:"url('../assets/Senku_test.png')", spriteRight:"url('../assets/Gen_test.png')"},
+    {line: "this line makes sure i can keep on moving with script", spriteLeft:"url('../assets/Senku_test.png')", spriteRight:"url('../assets/Gen_test.png')"},
+    {line: "and this is my last test for scene 0", spriteLeft:"url('../assets/Senku_test.png')", spriteRight:"url('../assets/Gen_test.png')"},
   ],
 
   [
     {line: "guess what? scene 2 starts here", spriteLeft:"url('../assets/Gen_test.png')", spriteRight:"url('../assets/Senku_test.png')"},
-    // {line: "You are far too nice, but it has a price", spriteLeft:"url('../assets/Gen_test.png')", spriteRight:"url('../assets/Senku_test.png')"},
-    // {line: "you reveal your name?", spriteLeft:"", spriteRight:"url('../assets/Senku_test.png')"},
-    // {line: "and you let him live.", spriteLeft:"url('../assets/Gen_test.png')", spriteRight:"url('../assets/Senku_test.png')"},
-    // {line: "ruthlessness is mercy upon ourselves", spriteLeft:"url('../assets/Gen_test.png')", spriteRight:"url('../assets/Senku_test.png')"},
-    // {line: "we took no pleasure in his pain", spriteLeft:"url('../assets/Gen_test.png')", spriteRight:"url('../assets/Senku_test.png')"},
-    // {line: "the line between naivety and hopefulness is almost invisible", spriteLeft:"url('../assets/Gen_test.png')", spriteRight:""},
+    {line: "You are far too nice, but it has a price", spriteLeft:"url('../assets/Gen_test.png')", spriteRight:"url('../assets/Senku_test.png')"},
+    {line: "you reveal your name?", spriteLeft:"", spriteRight:"url('../assets/Senku_test.png')"},
+    {line: "and you let him live.", spriteLeft:"url('../assets/Gen_test.png')", spriteRight:"url('../assets/Senku_test.png')"},
+    {line: "ruthlessness is mercy upon ourselves", spriteLeft:"url('../assets/Gen_test.png')", spriteRight:"url('../assets/Senku_test.png')"},
+    {line: "we took no pleasure in his pain", spriteLeft:"url('../assets/Gen_test.png')", spriteRight:"url('../assets/Senku_test.png')"},
+    {line: "the line between naivety and hopefulness is almost invisible", spriteLeft:"url('../assets/Gen_test.png')", spriteRight:""},
   ],
 
   [
     {line: "when does a ripple become a tidal wave", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
-    // {line: "when does a man become a monster", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
-    // {line: "I am your darkest moment", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
-    // {line: "All i gotta do is open this bag", spriteLeft:"", spriteRight: ""},
+    {line: "when does a man become a monster", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
+    {line: "I am your darkest moment", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
+    {line: "All i gotta do is open this bag", spriteLeft:"", spriteRight: ""},
   ],
 
   [
     {line: "captain. I have something that I must confess", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: ""},
-    // {line: "something that I must get off my chest", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
-    // {line: "go make sure this island is secure",spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
-    // {line: "where's the rest of your crew?", spriteLeft:"url('../assets/Gen_test.png')", spriteRight: "url('../assets/Senku_test.png')"},
-    // {line: "we came across a palace, inside we heard a voice", spriteLeft:"url('../assets/Gen_test.png')", spriteRight: "url('../assets/Senku_test.png')"},
-    // {line: "what did this palace hide?", spriteLeft:"url('../assets/Gen_test.png')", spriteRight: "url('../assets/Senku_test.png')"},
-    // {line: "but this was a hell of a twist, cause we are weak to a power like this", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
+    {line: "something that I must get off my chest", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
+    {line: "go make sure this island is secure",spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
+    {line: "where's the rest of your crew?", spriteLeft:"url('../assets/Gen_test.png')", spriteRight: "url('../assets/Senku_test.png')"},
+    {line: "we came across a palace, inside we heard a voice", spriteLeft:"url('../assets/Gen_test.png')", spriteRight: "url('../assets/Senku_test.png')"},
+    {line: "what did this palace hide?", spriteLeft:"url('../assets/Gen_test.png')", spriteRight: "url('../assets/Senku_test.png')"},
+    {line: "but this was a hell of a twist, cause we are weak to a power like this", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
   ],
 
   [
     {line: "I see a song of past romance", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
-    // {line: "I see the sacrifice of man", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
-    // {line: "I see portrayals of betrayal and a brother's final stand", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
+    {line: "I see the sacrifice of man", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
+    {line: "I see portrayals of betrayal and a brother's final stand", spriteLeft:"url('../assets/Senku_test.png')", spriteRight: "url('../assets/Gen_test.png')"},
 
   ],
 ]
@@ -45,7 +45,6 @@ const script= [
 // classes
 const itemElement = document.querySelectorAll(".item")
 const dialogueConstructionElement = document.querySelectorAll(".dialogue")
-const puzzleElement = document.querySelectorAll(".puzzle")
 const inventoryElement = document.querySelectorAll(".inventory")
 const SceneElements = document.querySelectorAll(".scenes")
 const mainRoomElement = document.querySelectorAll(".room1")
@@ -80,12 +79,10 @@ const winningScreenElement = document.querySelector("#winningScreen")
 const numberPuzzleElement = document.querySelectorAll(".number")
 
 /*-------------------------------- variables --------------------------------*/
-let currentScene, inventory, firstClickedItem, secondClickedItem, count, comboIsSet
+let inventory, firstClickedItem, secondClickedItem, count
 
 
 /*-------------------------------- Constants --------------------------------*/
-const key = {item:"fingerprint", img:"url('../assets/Gen_test.png')"}
-
 const clickableItems = [
   // MVP
   {item: "library", inventoryItem: false, willBeInventoryItem: false, scene: "0", background: librarySceneElement},
@@ -108,44 +105,6 @@ const clickableItems = [
   {item: "box", inventoryItem: false, willBeInventoryItem: false, scene: "7", background: boxSceneElement},
 
   {item: "numberPad", inventoryItem: false, willBeInventoryItem: false, scene: "4", background: numberPadElement},
-
-  // level up
-  {item: "book", inventoryItem: false, willBeInventoryItem: false, scene: "" },
-  {item: "paperWithAnswerKey", inventoryItem: true, scene: "" },
-
-  {item: "fridge", inventoryItem: false, willBeInventoryItem: false, scene: "" },
-  {item: "laptopPassword", inventoryItem: true, scene: "" },
-  {item: "fridgeCloseUp", inventoryItem: false, willBeInventoryItem: false, scene: "" },
-  {item: "coffee", inventoryItem: false, willBeInventoryItem: false, scene: "" },
-
-  {item: "bedroom", inventoryItem: false, willBeInventoryItem: false, scene: "" },
-  {item: "bed", inventoryItem: false, willBeInventoryItem: false, scene: "" },
-  {item: "xenoPhone", inventoryItem: false, willBeInventoryItem: false, scene: "" },
-]
-
-const puzzles = [
-  // MVP
-  {item: "briefCase", function: "briefCaseFunction", clickable: true},
-  {item: "fingerPrintReader"},
-  {item: "laptop", function: "laptopFunction", clickable: true},
-  {item: "numberPad", function: "numberPadFunction", clickable: true},
-
-  // level up
-  {item: "phone", function: "phoneFunction"},
-]
-
-const mergeableItems = [
-  // MVP
-  ["paperHalf_1", "paperHalf_2"],
-  ["fingerprintsKit", "keyboard"],
-  ["fingerprintsKit", "fingerPrintReader"],
-  ["fingerprintsKit", "numberPad"],
-  ["fingerprintReader", "fingerprint"],
-
-  // level up
-  ["fingerprintsKit", "coffee"],
-  ["fingerprintsKit", "xenoPhone"],
-  ["fingerprintsKit", "briefCase"],
 ]
 
 const inventoryItems = [
@@ -160,31 +119,15 @@ const briefCaseNumbers = [0, 0, 0, 0, 0, 0]
 // const briefCaseWinningCombo = [7, 3, 6, 4, 6, 8]
 const briefCaseWinningCombo = [1, 0, 0, 0, 0, 0]
 
-/*----------------------------- puzzle functions -----------------------------*/
+/*----------------------------- briefcase functions -----------------------------*/
 const unlockBriefCase = () => {
   show(briefCaseOpenElement)
   clickableItems[1].background=briefCaseOpenElement
 }
 
-const mapPuzzles = (functionId) =>{
-  if(functionId==="briefCaseFunction"){
-    briefCaseFunction()
-  }
-
-  if(functionId==="fingerPrintReaderFunction"){
-    fingerPrintReaderFunction()
-  }
-
-  if(functionId==="laptopFunction"){
-    laptopFunction()
-  }
-
-  if(functionId==="numberPadFunction"){
-    numberPadFunction()
-  }
-
-  if(functionId==="phoneFunction"){
-    phoneFunction()
+const fillBriefCase = () => {
+  for(i=0; i<6; i++){
+    numberPuzzleElement[i].textContent=briefCaseNumbers[i]
   }
 }
 
@@ -206,30 +149,11 @@ const briefCaseFunction = (idx) => {
   unlockBriefCase()
 }
 
-const fingerPrintReaderFunction = () => {
-  console.log("fingerPrintReaderFunction works");
-}
-
 /*-------------------------------- Functions --------------------------------*/
-
-const losingScreen = (()=>{
-  losingScreenElement.classList.remove("hide")
+const showEndScreen= ((result) =>{
+  result.classList.remove("hide")
   hide(dialogueConstructionElement)
   hide(itemElement)
-  hide(puzzleElement)
-  hide(inventoryElement)
-  hide(SceneElements)
-  hide(mainRoomElement)
-  hide(showBiggerBackgroundElement)
-  backButtonElement.classList.add("hide")
-  timeTrackerElement.classList.add("hide")
-})
-
-const winningScreen = (()=>{
-  winningScreenElement.classList.remove("hide")
-  hide(dialogueConstructionElement)
-  hide(itemElement)
-  hide(puzzleElement)
   hide(inventoryElement)
   hide(SceneElements)
   hide(mainRoomElement)
@@ -247,7 +171,7 @@ const timer = (()=>{
     }
     else if (count === 0) {
       clearInterval(timer);
-      losingScreen()
+      showEndScreen(losingScreenElement)
       return
     }
   }, 1000);
@@ -265,42 +189,27 @@ const show = (itemToShow) =>{
   });
 }
 
-const fillBriefCase = () => {
-  for(i=0; i<6; i++){
-    numberPuzzleElement[i].textContent=briefCaseNumbers[i]
-  }
+const resetClickedItems = () =>{
+  firstClickedItem= {name: "", idx:""}
 }
 
 const init = () =>{
-  // current scene is 0
-  currentScene = 0
-  // inventory is empty
   inventory = []
-  // clicked item 1
   firstClickedItem = {name: "", idx:""}
-  // clicked item 2
   secondClickedItem = {name: "", idx:""}
-  comboIsSet = false
   count = 600
-  // no dialogue
   hide(dialogueConstructionElement)
-  // no scenes
   hide(SceneElements)
-  // no back button
   backButtonElement.classList.add("hide")
   losingScreenElement.classList.add("hide")
   winningScreenElement.classList.add("hide")
-  // starts the timer
   timer()
-
   fillBriefCase()
-
 }
 
 init()
 
 const showInventory = () =>{
-  console.log(inventory, inventory.length);
   for(i=0; i<inventory.length; i++){
     inventoryElement[i+1].style.backgroundImage=inventory[i].img
   }
@@ -309,15 +218,15 @@ const showInventory = () =>{
   }
 }
 
-const addToInventory = (item) =>{
-  let inventoryItemsID = inventoryItems.findIndex((invItem)=>{
-    return invItem.item===item
+const addToInventory = (itemToAdd) =>{
+  let inventoryItemID = inventoryItems.findIndex((inventoryItem)=>{
+    return inventoryItem.item===itemToAdd
   })
 
-  inventory.push(inventoryItems[inventoryItemsID])
+  inventory.push(inventoryItems[inventoryItemID])
 
   itemElement.forEach(element => {
-    if(element.id===item){
+    if(element.id===itemToAdd){
       element.remove()
     }
   });
@@ -326,21 +235,18 @@ const addToInventory = (item) =>{
 }
 
 const mergeItems = (firstItem, secondItem)=>{
-  console.log("you're in");
   if(firstItem.name==="paperHalf_1" && secondItem.name==="paperHalf_2" || firstItem.name==="paperHalf_2" && secondItem.name==="paperHalf_1"){
     inventory.splice(firstItem.idx, 1)
     inventory.splice(secondItem.idx, 1)
     inventory.push(inventoryItems[2])
-    console.log(inventory);
   }
 
   if(firstItem.name==="fingerprintsKit" && secondItem.name==="keyboard"){
     inventory.push(inventoryItems[4])
-    console.log(inventory);
   }
 
   if(firstItem.name==="fingerprint" && secondItem.name==="fingerprintReader"){
-    winningScreen()
+    showEndScreen(winningScreenElement)
   }
 
   resetClickedItems()
@@ -348,34 +254,23 @@ const mergeItems = (firstItem, secondItem)=>{
 }
 
 const displayDialogue = (scenes, idx) =>{
-  // it will take the scene and the current line index, if we're not yet at the end of the scene, show the line
-  // and sprites
-  
   if(idx!=script[scenes].length){
     dialogueElement.textContent=script[scenes][idx].line
     leftSpriteElement.style.backgroundImage=script[scenes][idx].spriteLeft
     rightSpriteElement.style.backgroundImage=script[scenes][idx].spriteRight
   }
 
-  // if we're at the end of the scene, hide the dialogue elements and show the inventory, this is the part that
-  // ends recursion 
   if(idx===script[scenes].length){
     hide(dialogueConstructionElement)
     show(inventoryElement)
   }
 
-  // goes to the next line recursively
-  // when the next button is clicked, it will increase the line index and call this function again
   nextButtonElement.addEventListener("click", ()=>{
     if(idx<script[scenes].length){
       idx++
       displayDialogue(scenes, idx)
     }
   })
-}
-
-const resetClickedItems = () =>{
-  firstClickedItem= {name: "", idx:""}
 }
 
 const itemsWereClicked = (itemToSave, idx) =>{
@@ -444,9 +339,7 @@ const directDisplayRoom = (clickedItem) =>{
     return item.item===clickedItem.target.id
   })
 
-  // itemSceneID will save the id of the scene this item triggers
   itemSceneID=itemScene.scene
-  // it will also save the background of the scene
   itemSceneBackground=itemScene.background
   show(itemSceneBackground)
   hide(mainRoomElement)
@@ -454,32 +347,23 @@ const directDisplayRoom = (clickedItem) =>{
 }
 
 const directDisplayItem = (clickedItem) =>{
-  // it will find the specific item that was clicked by comparing the item name in the items array with the id
-  // of the clicked element
   let itemScene = clickableItems.find((item)=>{
     return item.item===clickedItem.target.id
   })
-
+  
   itemSceneID=itemScene.scene
-
-  // if this item has inventoryItem= true, it is not a scene element, don't continue and go to inventory with the
-  // name of the item
+  
   if(itemScene.inventoryItem){
     addToInventory(itemScene.item)
     return
   }
-
+  
   if(itemScene.willBeInventoryItem){
     itemScene.inventoryItem=true
   }
-
-  // it will hide the inventory and items
+  
   hide(inventoryElement)
-
-  // and show the dialogue and background of this scene and the backButton
-  show(dialogueConstructionElement)
-
-  // finally, it will activate the function that will progress the scene
+  show(dialogueConstructionElement)  
   displayDialogue(itemSceneID, 0)
 }
 
@@ -497,11 +381,10 @@ showBiggerBackgroundElement.forEach((element)=>{
     directDisplayRoom(event)
     element.classList.add("hide")
   })
-  
 })
 
-itemElement.forEach((item)=>{
-  item.addEventListener('click',(event)=>{
+itemElement.forEach((element)=>{
+  element.addEventListener('click',(event)=>{
     if(event.target.id==="keyboard"){
       itemsWereClicked("keyboard")
       return
@@ -519,7 +402,6 @@ itemElement.forEach((item)=>{
 
 backButtonElement.addEventListener("click", ()=>{
   resetClickedItems()
-  // it will hide dialogue, scenes and itself, and show elements and the inventory. 
   hide(dialogueConstructionElement)
   hide(SceneElements)
   show(itemElement)
